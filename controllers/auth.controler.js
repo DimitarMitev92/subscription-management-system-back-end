4a4b80b2e318d4f20784677ea77656ac7d56715c
Dimitar Mitev <d.mitev920123@gmail.com>
2025-03-11 17:24:34 +0200

    feat: sign up

---
+++ controllers/auth.controler.js
@@ -0,0 +1,65 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+import { JWT_SECRET, JWT_EXPIRES_IN } from "../config/env.js";
+
+export const signUp = async (req, res, next) => {
+  const session = await mongoose.startSession();
+  session.startTransaction();
+
+  try {
+    //Logic to create a new user
+    const { name, email, password } = req.body;
+
+    // Check if a user with the same email already exists
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      const error = new Error("User already exists");
+      error.statusCode = 409;
+      throw error;
+    }
+
+    // Hash the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create a new user
+    const newUsers = await user.create(
+      [{ name, email, password: hashedPassword }],
+      {
+        session,
+      }
+    );
+
+    // Generate JWT token
+    const token = jwt.sign({ userId: newUsers[0]._id }, JWT_SECRET, {
+      expiresIn: JWT_EXPIRES_IN,
+    });
+
+    await session.commitTransaction();
+    session.endSession();
+
+    res.status(201).json({
+      success: true,
+      message: "User created successfully",
+      data: { token, user: newUsers[0] },
+    });
+  } catch (error) {
+    await session.abortTransaction();
+    session.endSession();
+
+    next(error);
+  }
+};
+
+export const signIn = async (req, res, next) => {
+  // Implement your sign-in logic here
+};
+
+export const signOut = async (req, res, next) => {
+  // Implement your sign-out logic here
+};
+
+export default { signUp, signIn, signOut };
