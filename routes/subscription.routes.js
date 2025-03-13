a8a139c0298e6267c2ca2c0215a4a9a3c71e2ab2
Dimitar Mitev <d.mitev920123@gmail.com>
2025-03-13 16:40:34 +0200

    feat: user subscriptions

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,4 +1,9 @@
 import { Router } from "express";
+import authorize from "../middlewares/auth.middleware.js";
+import {
+  createSubscription,
+  getUserSubscriptions,
+} from "../controllers/subscription.controller.js";
 
 const subscriptionRouter = Router();
 
@@ -8,18 +13,15 @@ subscriptionRouter.get("/", (req, res) =>
 subscriptionRouter.get("/:id", (req, res) =>
   res.send({ title: "GET subscription details" })
 );
-subscriptionRouter.post("/", (req, res) =>
-  res.send({ title: "CREATE new subscription" })
-);
+subscriptionRouter.post("/", authorize, createSubscription);
+
 subscriptionRouter.put("/:id", (req, res) =>
   res.send({ title: "UPDATE subscription" })
 );
 subscriptionRouter.delete("/:id", (req, res) =>
   res.send({ title: "DELETE subscription" })
 );
-subscriptionRouter.get("/user/:id", (req, res) =>
-  res.send({ title: "GET all user subscriptions" })
-);
+subscriptionRouter.get("/user/:id", authorize, getUserSubscriptions);
 subscriptionRouter.put("/:id/cancel", (req, res) =>
   res.send({ title: "CANCEL subscription" })
 );
